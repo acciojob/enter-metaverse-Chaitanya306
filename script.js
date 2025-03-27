6b6cb85b5c9df418cfdbc36e78ab1db1bdc7adc4
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-27 15:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const p=document.getElementById("status")
+const button=document.getElementById("enterBtn")
+const h1=document.getElementsByTagName("h1")
+
+function clicked(){
+	p.style.display='none'
+	h1[0].style.display='block'
+}

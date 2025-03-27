04d67d8ec7044abd2c684d1b09f5f5dfc7115537
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-27 15:24:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If required.
 const p=document.getElementById("status")
 const button=document.getElementById("enterBtn")
-const h1=document.getElementsByTagName("h1")
+//const h1=document.getElementsByTagName("h1")
 
 function clicked(){
-	p.style.display='none'
-	h1[0].style.display='block'
+	p.innerHTML="<h1>Entered Metaverse</h1>"
 }
